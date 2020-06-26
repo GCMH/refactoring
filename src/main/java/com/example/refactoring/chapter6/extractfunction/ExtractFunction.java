184907ba417cd85ebff5fe568f9889e55650e469
GCMH <huang.changfan@qq.com>
2020-06-26 16:25:07 +0800

    6.1提炼函数-有局部变量-提炼函数

--- src/main/java/com/example/refactoring/chapter6/extractfunction/ExtractFunction.java
+++ src/main/java/com/example/refactoring/chapter6/extractfunction/ExtractFunction.java
@@ -13,26 +13,38 @@ public class ExtractFunction {
         invoice.orders = new Order[]{new Order(5), new Order(10)};
         System.out.println(printOwing(invoice));
     }
+
     public static String printOwing(Invoice invoice){
         StringBuffer result = new StringBuffer();
         int outstanding = 0;
-        result.append("*******************\n");
-        result.append("***Customer Owes***\n");
-        result.append("*******************\n");
+        pringBanner(result);
+        outstanding = getOutstanding(invoice, outstanding);
+        getDueDate(invoice);
+        printDetails(invoice, result, outstanding);
+        return result.toString();
+    }
 
-        //calculate outstanding
+    public static void getDueDate(Invoice invoice) {
+        LocalDate today = LocalDate.now();
+        invoice.dueDate = today.plusDays(30);
+    }
+
+    public static int getOutstanding(Invoice invoice, int outstanding) {
         for(Order o: invoice.orders){
             outstanding += o.amount;
         }
+        return outstanding;
+    }
 
-        LocalDate today = LocalDate.now();
-        invoice.dueDate = today.plusDays(30);
-
-        //print details
+    public static void printDetails(Invoice invoice, StringBuffer result, int outstanding) {
         result.append("name:" + invoice.customer + "\n");
         result.append("amount:" + outstanding + "\n");
         result.append("due:" + invoice.dueDate.toString() + "\n");
+    }
 
-        return result.toString();
+    public static void pringBanner(StringBuffer result) {
+        result.append("*******************\n");
+        result.append("***Customer Owes***\n");
+        result.append("*******************\n");
     }
 }
