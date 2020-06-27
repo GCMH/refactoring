4af5c864faf237a4ddf1ca81f6e997bbce47c39a
GCMH <huang.changfan@qq.com>
2020-06-27 23:11:30 +0800

    6.5改变函数声明-添加函数参数-添加函数参数

--- src/main/java/com/example/refactoring/chapter6/ChangeFunctionDeclaratio/Book.java
+++ src/main/java/com/example/refactoring/chapter6/ChangeFunctionDeclaratio/Book.java
@@ -8,6 +8,10 @@ public class Book {
     public List<Customer> _reservations = new ArrayList<>();
 
     public void addReservation(Customer customer){
+        zz_addReservation(customer, false);
+    }
+
+    public void zz_addReservation(Customer customer, boolean isPro){
         this._reservations.add(customer);
     }
 }
