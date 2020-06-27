8e7483a1b15173f84fafe22511649aeab296e33d
GCMH <huang.changfan@qq.com>
2020-06-27 22:57:24 +0800

    6.5改变函数声明-源代码

---
+++ src/main/java/com/example/refactoring/chapter6/ChangeFunctionDeclaratio/ChangeFunctionDeclaratio.java
@@ -0,0 +1,13 @@
+package com.example.refactoring.chapter6.ChangeFunctionDeclaratio;
+
+public class ChangeFunctionDeclaratio {
+
+
+    public static void main(String[] args) {
+        System.out.println(circum(2.3f));
+    }
+
+    public static double circum(double r){
+        return  2 * Math.PI * r;
+    }
+}
