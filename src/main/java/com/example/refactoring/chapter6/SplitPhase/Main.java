5ea6b9ebcc3636acc72c2ce5d24e12ee778653c0
GCMH <huang.changfan@qq.com>
2020-07-01 23:18:05 +0800

    6.11拆分阶段-拆分方法为阶段

--- src/main/java/com/example/refactoring/chapter6/SplitPhase/Main.java
+++ src/main/java/com/example/refactoring/chapter6/SplitPhase/Main.java
@@ -9,13 +9,39 @@ public class Main {
 
 
     public static double priceOrder(Product product, double quantity, ShippingMethod shippingMethod) {
+
+
+        PriceData priceData = calculatePricingData(product, quantity);
+
+
+        double shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
+                ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+
+
+        double shippingCost = quantity * shippingPerCase;
+
+        double price = priceData.basePrice - priceData.discount + shippingCost;
+        return price;
+    }
+
+
+    public static PriceData calculatePricingData(Product product, double quantity){
         double basePrice = product.basePrice * quantity;
         double discount = Math.max(quantity - product.discountThreshold, 0)
                 * product.basePrice * product.discountRate;
-        double shippingPerCase = (basePrice > shippingMethod.discountThreshold)
-                ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-        double shippingCost = quantity * shippingPerCase;
-        double price = basePrice - discount + shippingCost;
-        return price;
+
+        PriceData priceData = new PriceData();
+        priceData.basePrice = basePrice;
+        priceData.discount = discount;
+        priceData.quantity = quantity;
+        return priceData;
     }
+
+    public static double applyShipping(PriceData priceData, ShippingMethod shippingMethod){
+        double shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
+                ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+        double shippingCost = priceData.quantity * shippingPerCase;
+        return priceData.basePrice - priceData.discount + shippingCost;
+    }
+
 }
---
+++ src/main/java/com/example/refactoring/chapter6/SplitPhase/PriceData.java
@@ -0,0 +1,7 @@
+package com.example.refactoring.chapter6.SplitPhase;
+
+public class PriceData {
+    public double basePrice;
+    public double discount;
+    public double quantity;
+}
