0136777461214dc9a42cd4d552e6789952386f7e
GCMH <huang.changfan@qq.com>
2020-07-05 23:53:20 +0800

    7.6隐藏委托关系-将委托关系通过委托函数隐藏调用

--- src/main/java/com/example/refactoring/chapter6/HideDelegate/Main.java
+++ src/main/java/com/example/refactoring/chapter6/HideDelegate/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person();
 
-        System.out.println(person.department.manager);
+        System.out.println(person.getManager());
     }
 }
