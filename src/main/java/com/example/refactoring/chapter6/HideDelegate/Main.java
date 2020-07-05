339affabd3647e49f64fa9046379a85692d7529a
GCMH <huang.changfan@qq.com>
2020-07-05 23:51:32 +0800

    7.6隐藏委托关系-源代码

---
+++ src/main/java/com/example/refactoring/chapter6/HideDelegate/Main.java
@@ -0,0 +1,9 @@
+package com.example.refactoring.chapter6.HideDelegate;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        System.out.println(person.department.manager);
+    }
+}
---
+++ src/main/java/com/example/refactoring/chapter6/HideDelegate/Person.java
@@ -0,0 +1,24 @@
+package com.example.refactoring.chapter6.HideDelegate;
+
+public class Person {
+
+    String name;
+
+    Department department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
