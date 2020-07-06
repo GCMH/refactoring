f2f7b54f5de513ee8ed1df1c34de0f0e165a7982
GCMH <huang.changfan@qq.com>
2020-07-06 22:05:52 +0800

    8.1搬移函数-源代码

---
+++ src/main/java/com/example/refactoring/chapter6/MoveFunction/Account.java
@@ -0,0 +1,28 @@
+package com.example.refactoring.chapter6.MoveFunction;
+
+public class Account {
+
+    private double daysOverdrawn;
+    private int _daysOverdrawn;
+    private double overdraftCharge;
+    private AccountType type;
+
+    double getBankCharge() {
+        double result = 4.5;
+        if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+        return result;
+    }
+
+    double getOverdraftCharge() {
+        if (this.type.isPremium) {
+            int baseCharge = 10;
+            if (this.daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+        } else {
+            return this.daysOverdrawn * 1.75;
+        }
+
+    }
+}
---
+++ src/main/java/com/example/refactoring/chapter6/MoveFunction/AccountType.java
@@ -0,0 +1,19 @@
+package com.example.refactoring.chapter6.MoveFunction;
+
+public class AccountType {
+
+    public boolean isPremium;
+
+
+    double overdraftCharge(double daysOverdrawn) {
+        if (this.isPremium) {
+            int baseCharge = 10;
+            if (daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+        } else {
+            return daysOverdrawn * 1.75;
+        }
+    }
+}
