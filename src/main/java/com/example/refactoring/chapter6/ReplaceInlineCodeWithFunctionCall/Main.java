4a82bbb602111587bdcfb7dcab265fb9d9d76e2e
GCMH <huang.changfan@qq.com>
2020-07-08 22:20:12 +0800

    8.5以函数调用取代内联代码-提取内联代码-使用函数取代

--- src/main/java/com/example/refactoring/chapter6/ReplaceInlineCodeWithFunctionCall/Main.java
+++ src/main/java/com/example/refactoring/chapter6/ReplaceInlineCodeWithFunctionCall/Main.java
@@ -7,15 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("AA", "BB");
-
-        boolean flag = false;
-        for (String s : list) {
-            if(s.equals("AA"))
-                flag = true;
-        }
-
-
+        boolean flag = list.contains("AA");
 
     }
-
 }
