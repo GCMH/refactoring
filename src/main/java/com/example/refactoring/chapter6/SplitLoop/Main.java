a75e7b343224fbc1d102844a86fe1d5d7938b658
GCMH <huang.changfan@qq.com>
2020-07-08 22:38:48 +0800

    8.7拆分循环-将每个循环相关上下文组织在一起-提炼函数

--- src/main/java/com/example/refactoring/chapter6/SplitLoop/Main.java
+++ src/main/java/com/example/refactoring/chapter6/SplitLoop/Main.java
@@ -5,16 +5,21 @@ public class Main {
     public static void main(String[] args) {
 
         People[] people = new People[5];
+        calTotalSalary(people);
+        getMinAge(people);
+    }
 
-
-        int totalSalary = 0;
-        for (People p : people) {
-            totalSalary += p.salary;
-        }
-
+    public static void getMinAge(People[] people) {
         int youngest = people[0] != null ? people[0].age : Infinity;
         for (People p : people) {
             if (p.age < youngest) youngest = p.age;
+        }
+    }
+
+    public static void calTotalSalary(People[] people) {
+        int totalSalary = 0;
+        for (People p : people) {
+            totalSalary += p.salary;
         }
     }
 }
