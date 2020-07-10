d8e8a33764ed7a06bc21b2f553bacf1261cb19cc
GCMH <huang.changfan@qq.com>
2020-07-10 21:19:27 +0800

    8.9拆分变量-变量声明为final-不同变量表示不同职责-重复赋值部分添加新变量

--- src/main/java/com/example/refactoring/chapter6/SplitVariable/Main.java
+++ src/main/java/com/example/refactoring/chapter6/SplitVariable/Main.java
@@ -3,20 +3,36 @@ package com.example.refactoring.chapter6.SplitVariable;
 public class Main {
     public static void main(String[] args) {
 
-
         Scenario scenario = new Scenario();
-
         scenario.delay = 0.01;
         scenario.mass = 0.02;
         scenario.primaryForce = 3.1;
         scenario.secondaryForce = 3.4;
 
+        assert distanceTravelled(scenario, 0.3) == distanceTravelled(scenario, 0.3);
 
 
     }
 
 
-    double distanceTravelled (Scenario scenario, double time) {
+    static double distanceTravelled (Scenario scenario, double time) {
+        double result;
+
+
+        final double primaryAcceleration = scenario.primaryForce / scenario.mass;
+        double primaryTime = Math.min(time, scenario.delay);
+        result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
+        double secondaryTime = time - scenario.delay;
+        if (secondaryTime > 0) {
+            double primaryVelocity = primaryAcceleration * scenario.delay;
+            final double secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+            result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
+        }
+        return result;
+    }
+
+
+    static double distanceTravelledTest (Scenario scenario, double time) {
         double result;
         double acc = scenario.primaryForce / scenario.mass;
         double primaryTime = Math.min(time, scenario.delay);
