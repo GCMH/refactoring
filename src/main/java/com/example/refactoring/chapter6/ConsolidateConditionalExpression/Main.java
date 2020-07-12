9d94277849136216c666d2002a6b5acb1dc858ec
GCMH <huang.changfan@qq.com>
2020-07-12 21:49:01 +0800

    8.13-合并条件表达式-将多个最终行为相同的条件表达式组合成与或的形式

--- src/main/java/com/example/refactoring/chapter6/ConsolidateConditionalExpression/Main.java
+++ src/main/java/com/example/refactoring/chapter6/ConsolidateConditionalExpression/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
 
     public static int disabilityAmount(Employee anEmployee){
-        if (anEmployee.seniority < 2) return 0;
-        if (anEmployee.monthsDisabled > 12) return 0;
-        if (anEmployee.isPartTime) return 0;
+        if (anEmployee.seniority < 2
+                || anEmployee.monthsDisabled > 12
+                || anEmployee.isPartTime) return 0;
         return -1;
     }
 }
